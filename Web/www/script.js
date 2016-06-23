4d788a1bd510ed4457e665acb5e95629fa38d368
K <kbecker@primaryobjects.com>
2016-06-22 22:41:46 -0400

    adblock fix

--- Web/www/script.js
+++ Web/www/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     $('#btnUrl').click(function() {
-        if (ga) {
+        if (typeof(ga) != 'undefined') {
             ga('send', 'event', 'btnUrl', 'click', $('#url').val());
         }
     });
 
     $('#file1').click(function() {
-        if (ga) {
+        if (typeof(ga) != 'undefined') {
             ga('send', 'event', 'file1', 'click');
         }
     });
