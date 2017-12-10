3945a30eeb21a1f81c0ddb28789753ee1274b676
Kory Becker <kbecker@primaryobjects.com>
2017-12-09 21:39:45 -0500

    Added register option.

--- Web/ui.R
+++ Web/ui.R
@@ -11,12 +11,56 @@ shinyUI(fluidPage(
   conditionalPanel(condition='!output.json',
                    tags$head(tags$script(src = "script.js"),
                    			 tags$script(src = "google-analytics.js"),
-                         tags$style(HTML("a { font-weight: bold; } .shiny-output-error-validation { color: red; } .shiny-progress .progress { background-color: #ff00ff; } .fa-info { margin: 0 0 0 10px; cursor: pointer; font-size: 15px; color: #808080; } .fa-headphones { margin: 0 5px 0 2px; } .average-pitch { font-size: 18px; } .detail-summary { font-size: 16px; } .detail-summary .detail-header { font-size: 18px; margin: 0 0 10px 0; } .detail-summary span { font-weight: bold; } #summary1 { margin-top: 10px; } #summary2 { margin-bottom: 80px; } .female { color: #ff00ff; } .male { color: #0066ff; } .error { color: #ff0000; }"))
+                         tags$style(HTML("a { font-weight: bold; } .shiny-output-error-validation { color: red; } .shiny-progress .progress { background-color: #ff00ff; } .fa-info { margin: 0 0 0 10px; cursor: pointer; font-size: 15px; color: #808080; } .fa-headphones { margin: 0 5px 0 2px; } .average-pitch { font-size: 18px; } .detail-summary { font-size: 16px; } .detail-summary .detail-header { font-size: 18px; margin: 0 0 10px 0; } .detail-summary span { font-weight: bold; } #summary1 { margin-top: 10px; } #summary2 { margin-bottom: 80px; } .female { color: #ff00ff; } .male { color: #0066ff; } .error { color: #ff0000; } .btn-primary.buy { margin: 0 0 10px 10px; border-radius: 4px; padding: 8px 12px 8px 12px; border: 0; -webkit-transition: all .25s ease-in-out; } .btn-primary.buy:hover { background-color: #e0e0e0; color: #333; } .intro-text { font-weight: bolder; font-size: 16px; color: deeppink; } .intro-text.register { color: #00aa00; } #lstLicense { margin-top: 3px; } #register-div { border: 1px solid #c0c0c0; padding: 10px 15px 10px 15px; max-width: 422px; background-color: aliceblue; } .fa-info-circle { cursor: pointer; -webkit-transition: all .1s ease-in-out; } .fa-info-circle:hover { color: #333; }"))
                    ),
                    titlePanel('What is Your Voice Gender?'),
-                   div(style='margin: 30px 0 0 0;'),
-  mainPanel(width = '100%',
+   mainPanel(width = '100%',
    useShinyjs(),
+   
+   tags$form(action="https://www.paypal.com/cgi-bin/webscr", method="post", name="m_OrderForm", id="m_OrderForm",
+     tags$input(type="hidden", name="business", value="kbecker@primaryobjects.com"),
+     tags$input(type="hidden", name="item_name", value="Voice Gender App"),
+     tags$input(type="hidden", name="item_number", value="VOICEGENDERAPP"),
+     tags$input(type="hidden", name="amount", value="19.95"),
+     tags$input(type="hidden", name="no_shipping", value="1"),
+     tags$input(type="hidden", name="return", value="https://primaryobjects.shinyapps.io/voice/?action=register"),
+     tags$input(type="hidden", name="currency_code", value="USD"),
+     tags$input(type="hidden", name="no_note", value="1"),
+     tags$input(type="hidden", name="cancel_return", value="https://primaryobjects.shinyapps.io/voice/?action=cancel"),
+     tags$input(type="hidden", name="cmd", value="_xclick")
+   ),
+
+   div(id="register-div",
+     div(
+       div(style="float: left;",
+         p(class="intro-text",
+           "Register today to support the Voice Gender App!"
+         )
+       ),
+       div(id="info",
+         tags$i(class="fa fa-info-circle", style="float: left; margin: 2px 0 0 6px;", "data-toggle"="tooltip", "data-placement"="bottom", title="We need your help! This app uses a method of artificial intelligence, called machine learning, to determine the gender of a voice. Unfortunately, it's not cheap to host this thing, and the app will go down if we're unable to keep it hosted online. :( You can help by registering for the site to allow us to order a hosting plan!")
+       )
+     ),
+     div(style="clear: both;"),
+     
+     div(id="license-div",
+       div(style="float: left",
+         tags$select(id="lstLicense", class="form-control",
+           tags$option(value="Starter", "Starter License $9.95"),
+           tags$option(value="Personal", selected="true", "Personal License $19.95"),
+           tags$option(value="Professional", "Professional License $39.95"),
+           tags$option(value="Gold", "Gold License $99.95")
+         )
+       ),
+       div(
+         a(href="#", class="btn btn-primary btn-lg submit buy",
+           tags$i(class="fa fa-unlock-alt"),
+           "Buy Now"
+         )
+       )
+     )
+   ),
+     
    h4(id='main', 'Upload a .WAV file of your voice or enter a url from ', a(href='http://vocaroo.com', target='_blank', 'vocaroo.com'), 'or ', a(href='http://clyp.it', target='_blank', 'clyp.it'), ' to detect its gender.'),
    div(style='margin: 20px 0 0 0;'),
    
--- Web/www/script.js
+++ Web/www/script.js
@@ -17,4 +17,67 @@ $(document).ready(function() {
             $("#btnUrl").click();
         }
     });
+
+    // Activate tooltips.
+    $('body').tooltip({ selector: '[data-toggle=tooltip]' });
+
+    // Setup form submit event.
+    $('.submit').click(function() {
+        $(this).css('cursor', 'wait');
+
+        // Log event.
+        ga('send', 'event', 'buy', $('#lstLicense').val(), 'click');
+
+        $('#m_OrderForm').submit();
+        
+        return false;
+    });
+
+    $('#lstLicense').change(function() {
+        // Log event.
+        ga('send', 'event', 'license', $(this).val(), 'change');
+
+        onLicense();
+    });
+
+    // Initialize intro text.
+    var action = getUrlParameter('action');
+    if (action == 'register') {
+        $('.intro-text').addClass(action).text('Thank you for purchasing a license!');
+        $('#license-div').hide();
+    }
+
+    // Page initialization.
+    onLicense();
 });
+
+function onLicense() {
+    var element = $('#lstLicense'); // Get select control.
+    var selectedValue = element.val(); // Get selected value.
+    var selectedText = element.find('option:selected').text() // Get selected text including price.
+    var d = 0; // Get any discount.
+    var price = (selectedText.match(/\$([0-9,.]+)/)[1] - d).toFixed(2); // Extract price value from text.
+    var license = selectedText.match(/^([\w]+)/)[1]; // Extract license name by first-word.
+    var licenseName = 'Voice Gender App, ' + license; // Setup full license name.
+    var code = 'VOICEGENDERAPP' + license.substr(0, 3).toUpperCase(); // Setup product code, using first 3 letters in license, upper-case (PER, BUS).
+    
+    // Update price, item number, and item name.
+    $("#m_OrderForm input[name='amount']").val(price);
+    $("#m_OrderForm input[name='item_number']").val(code);
+    $("#m_OrderForm input[name='item_name']").val(licenseName);
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
