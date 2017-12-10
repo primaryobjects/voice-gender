09d7c2b77601468660ff5e56c57ad5a77fa7f82d
Kory Becker <kbecker@primaryobjects.com>
2017-12-09 21:52:25 -0500

    Text.

--- Web/ui.R
+++ Web/ui.R
@@ -38,7 +38,7 @@ shinyUI(fluidPage(
          )
        ),
        div(id="info",
-         tags$i(class="fa fa-info-circle", style="float: left; margin: 2px 0 0 6px;", "data-toggle"="tooltip", "data-placement"="bottom", title="We need your help! This app uses a method of artificial intelligence, called machine learning, to determine the gender of a voice. Unfortunately, it's not cheap to host this thing, and the app will go down if we're unable to keep it hosted online. :( You can help by registering for the site to allow us to order a hosting plan!")
+         tags$i(class="fa fa-info-circle", style="float: left; margin: 2px 0 0 6px;", "data-toggle"="tooltip", "data-placement"="bottom", title="This app uses a method of artificial intelligence, called machine learning, to determine the gender of a voice. Unfortunately, it's not cheap to host this thing. :( Register for the Voice Gender App to help keep us online! Registered users receive priority email support and priority suggestions for new features.")
        )
      ),
      div(style="clear: both;"),
