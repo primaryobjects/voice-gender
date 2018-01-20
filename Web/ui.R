9e98e557f6873148f49a285b47b3966dab1804d9
Kory Becker <kbecker@primaryobjects.com>
2018-01-19 20:44:01 -0500

    Update ui.R

--- Web/ui.R
+++ Web/ui.R
@@ -5,7 +5,7 @@
 #}
 
 library(shiny)
-library(shinyjs)
+#library(shinyjs)
 
 shinyUI(fluidPage(
   conditionalPanel(condition='!output.json',
@@ -14,8 +14,7 @@ shinyUI(fluidPage(
                          tags$style(HTML("a { font-weight: bold; } .shiny-output-error-validation { color: red; } .shiny-progress .progress { background-color: #ff00ff; } .fa-info { margin: 0 0 0 10px; cursor: pointer; font-size: 15px; color: #808080; } .fa-headphones { margin: 0 5px 0 2px; } .average-pitch { font-size: 18px; } .detail-summary { font-size: 16px; } .detail-summary .detail-header { font-size: 18px; margin: 0 0 10px 0; } .detail-summary span { font-weight: bold; } #summary1 { margin-top: 10px; } #summary2 { margin-bottom: 80px; } .female { color: #ff00ff; } .male { color: #0066ff; } .error { color: #ff0000; } .btn-primary.buy { margin: 0 0 10px 10px; border-radius: 4px; padding: 8px 12px 8px 12px; border: 0; -webkit-transition: all .25s ease-in-out; } .btn-primary.buy:hover { background-color: #e0e0e0; color: #333; } .intro-text { font-weight: bolder; font-size: 16px; color: deeppink; } .intro-text.register { color: #00aa00; } #lstLicense { margin-top: 3px; } #register-div { border: 1px solid #c0c0c0; padding: 10px 15px 10px 15px; max-width: 422px; background-color: aliceblue; } .fa-info-circle { cursor: pointer; -webkit-transition: all .1s ease-in-out; } .fa-info-circle:hover { color: #333; }"))
                    ),
                    titlePanel('What is Your Voice Gender?'),
-   mainPanel(width = '100%',
-   useShinyjs(),
+   mainPanel(width = '100%'),
    
    tags$form(action="https://www.paypal.com/cgi-bin/webscr", method="post", name="m_OrderForm", id="m_OrderForm",
      tags$input(type="hidden", name="business", value="kbecker@primaryobjects.com"),
