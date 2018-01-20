a42be282cdb2af9f6212fe8b910bd3bdf055ebb1
Kory Becker <kbecker@primaryobjects.com>
2018-01-19 21:25:51 -0500

    Updated run.R

--- run.R
+++ run.R
@@ -1,5 +1,10 @@
 library(shiny)
 
+packages <- c('shiny', 'shinyjs', 'RJSONIO', 'RCurl', 'warbleR', 'tuneR', 'seewave', 'gbm', 'xgboost', 'randomForest', 'e1071')
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+	install.packages(setdiff(packages, rownames(installed.packages())))  
+}
+
 port <- Sys.getenv('PORT')
 
 shiny::runApp(
