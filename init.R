e45f5026186cb57f9bc5617deebfd04ec2871349
Kory Becker <kbecker@primaryobjects.com>
2018-07-02 17:21:26 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,4 +1,5 @@
-my_packages = c("RJSONIO", "RCurl", "warbleR", "parallel", "tuneR", "seewave", "gbm", "xgboost", "randomForest", "e1071")
+#my_packages = c("RJSONIO", "RCurl", "warbleR", "parallel", "tuneR", "seewave", "gbm", "xgboost", "randomForest", "e1071")
+my_packages = c("RJSONIO", "RCurl", "parallel", "tuneR", "seewave", "gbm", "xgboost", "randomForest", "e1071")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
@@ -7,3 +8,6 @@ install_if_missing = function(p) {
 }
 
 invisible(sapply(my_packages, install_if_missing))
+
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/warbleR/warbleR_1.1.9.tar.gz"
+install.packages(packageurl, repos=NULL, type="source")
