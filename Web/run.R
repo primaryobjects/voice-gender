476338a81fc83b4f613a8afe23abba73dbdc3768
Kory Becker <kbecker@primaryobjects.com>
2022-12-19 21:50:12 -0500

    Updated vscode to autodetect.

--- Web/run.R
+++ Web/run.R
@@ -1,13 +1,19 @@
-# Stub for running within VSCode. Uncomment the following lines to run.
-# library(shiny)
+# Stub for running within VSCode debugger.
+isDebugMode <- Sys.getenv("VS_DEBUG") == "1"
+if (isDebugMode) {
+    print("Running in VSCode debug mode.")
+    library(shiny)
 
-# runApp(
-#   appDir = getwd(),
-#   port = getOption("shiny.port"),
-#   launch.browser = getOption("shiny.launch.browser", interactive()),
-#   host = getOption("shiny.host", "127.0.0.1"),
-#   workerId = "",
-#   quiet = FALSE,
-#   display.mode = c("auto", "normal", "showcase"),
-#   test.mode = getOption("shiny.testmode", FALSE)
-# )
+    runApp(
+    appDir = getwd(),
+    port = getOption("shiny.port"),
+    launch.browser = getOption("shiny.launch.browser", interactive()),
+    host = getOption("shiny.host", "127.0.0.1"),
+    workerId = "",
+    quiet = FALSE,
+    display.mode = c("auto", "normal", "showcase"),
+    test.mode = getOption("shiny.testmode", FALSE)
+    )
+} else {
+    print("Running in web server mode.")
+}
