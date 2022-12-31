23af60531847bf8593d085ffd627f4d1e3052829
Kory Becker <kbecker@primaryobjects.com>
2022-12-31 12:01:06 -0500

    Updated Google Analytics to GA4.

--- Web/www/google-analytics.js
+++ Web/www/google-analytics.js
@@ -1,7 +1,5 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-ga('create', 'UA-843113-30', 'auto');
-ga('send', 'pageview');
+gtag('config', 'G-5Z96YHFEDG');
